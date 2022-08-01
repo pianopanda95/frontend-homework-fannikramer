079a1cf502cb0717f22c013243604c72af8519a6
pianopanda95 <kramerfanni95@gmail.com>
2022-08-01 18:30:08 +0200

    clean up existing code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 const username = 'user3472';
 const apiKey = '0f6aa487-0f3b-41dc-95be-86c19dd0b98d';
 
+const fetchObject = {
+    mode: 'cors',
+    credentials: 'include',
+    method: 'GET',
+    headers: {
+        'Authorization': 'Basic ' + btoa(`${username}:${apiKey}`),
+    },
+}
+
+
 const brandInput = document.querySelector('#brand');
 const dataListBrands = document.querySelector(`datalist#brands`);
-
 const categorySelect = document.querySelector('#category');
+
 
 const displaySearchResults = (fetchedObj, attributeName, parent) => {
     const suggestion = document.createElement('option');
@@ -13,46 +23,36 @@ const displaySearchResults = (fetchedObj, attributeName, parent) => {
     parent.appendChild(suggestion);
 }
 
-brandInput.addEventListener('input', () => {
-
+const clearCategories = () => {
     if (categorySelect.children.length > 1) {
         const categoryNodes = Array.from(categorySelect.children);
         categoryNodes.shift();
         categoryNodes.forEach(node => node.remove());
     }
+}
 
-    fetch(`https://size-calculator-api.sspinc.io/brands?name_prefix=${brandInput.value}`, {
-        mode: 'cors',
-        credentials: 'include',
-        method: 'GET',
-        headers: {
-            'Authorization': 'Basic ' + btoa(`${username}:${apiKey}`)
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        dataListBrands.innerHTML = '';
-        data.brands.map(brand => displaySearchResults(brand, 'data', dataListBrands));
-    })
-    .catch(error => console.log(error)); 
+brandInput.addEventListener('input', () => {
+
+    clearCategories()
+
+    fetch(`https://size-calculator-api.sspinc.io/brands?name_prefix=${brandInput.value}`, fetchObject)
+        .then(response => response.json())
+        .then(data => {
+            dataListBrands.innerHTML = '';
+            data.brands.map(brand => displaySearchResults(brand, 'data', dataListBrands));
+        })
+        .catch(error => console.log(error)); 
 });
 
 
 categorySelect.addEventListener('focus', () => {
 
     const selectedBrandId = dataListBrands.firstChild.getAttribute('data');
-        fetch(`https://size-calculator-api.sspinc.io/categories?brand_id=${selectedBrandId}`, {
-            mode: 'cors',
-            credentials: 'include',
-            method: 'GET',
-            headers: {
-                'Authorization': 'Basic ' + btoa(`${username}:${apiKey}`)
-            }
-        })
+
+    fetch(`https://size-calculator-api.sspinc.io/categories?brand_id=${selectedBrandId}`, fetchObject)
         .then(response => response.json())
-        .then(data => {
-            data.categories.map(category => displaySearchResults(category, 'value', categorySelect));
-        })
-    .catch(error => console.log(error)); 
+        .then(data => data.categories.map(category =>
+                      displaySearchResults(category, 'value', categorySelect)))
+        .catch(error => console.log(error)); 
 });
 

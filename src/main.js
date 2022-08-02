c4b917598ceff00eb577d386fbe4cb75c328e37c
pianopanda95 <kramerfanni95@gmail.com>
2022-08-02 17:05:00 +0200

    add createResultPage and specify steps for calculateButton's fetch

--- src/main.js
+++ src/main.js
@@ -14,6 +14,8 @@ const fetchObject = {
 const brandInput = document.querySelector('#brand');
 const dataListBrands = document.querySelector(`datalist#brands`);
 const categorySelect = document.querySelector('#category');
+const size = document.querySelector('input[type="number"]');
+const calculateButton = document.querySelector('button');
 
 
 const displaySearchResults = (fetchedObj, attributeName, parent) => {
@@ -41,18 +43,54 @@ brandInput.addEventListener('input', () => {
             dataListBrands.innerHTML = '';
             data.brands.map(brand => displaySearchResults(brand, 'data', dataListBrands));
         })
-        .catch(error => console.log(error)); 
+        .catch(error => console.log(error));
 });
 
 
 categorySelect.addEventListener('focus', () => {
 
-    const selectedBrandId = dataListBrands.firstChild.getAttribute('data');
+    let selectedBrandId = dataListBrands.firstChild.getAttribute('data');
 
-    fetch(`https://size-calculator-api.sspinc.io/categories?brand_id=${selectedBrandId}`, fetchObject)
-        .then(response => response.json())
-        .then(data => data.categories.map(category =>
-                      displaySearchResults(category, 'value', categorySelect)))
-        .catch(error => console.log(error)); 
+    if (categorySelect.children.length <= 1) {
+        fetch(`https://size-calculator-api.sspinc.io/categories?brand_id=${selectedBrandId}`, fetchObject)
+            .then(response => response.json())
+            .then(data => data.categories.map(category =>
+                        displaySearchResults(category, 'value', categorySelect)))
+            .catch(error => console.log(error));
+    }
 });
 
+const createResultPage = result =>{
+    const fieldset = document.querySelector('fieldset');
+
+    const fieldsetNodes = Array.from(fieldset.children);
+    fieldsetNodes.forEach(node => node.remove());
+
+    const firstLine = document.createElement('p');
+    firstLine.textContent = 'Your size is';
+
+    const resultLine = document.createElement('p');
+    resultLine.classList.add('result');
+    resultLine.textContent = result;
+
+    const okButton = document.createElement('button');
+    okButton.textContent = 'OK';
+
+    fieldset.appendChild(firstLine);
+    fieldset.appendChild(resultLine);
+    fieldset.appendChild(okButton);
+}
+calculateButton.addEventListener('click', (e) => {
+    e.preventDefault(); //temporary
+    let selectedBrandId = dataListBrands.firstChild.getAttribute('data');
+    const selectedCategoryId = document.querySelector('#category').value;
+
+    fetch(`https://size-calculator-api.sspinc.io/sizes?brand_id=${selectedBrandId}&category_id=${selectedCategoryId}&measurement=${size.value}`, fetchObject)
+    .then(response => response.json())
+    .then(data => {
+        const result = data.sizes.length ?
+            data.sizes.map(size => size.label).join(' or '):
+            `No size found for these parameters`;
+        createResultPage(result);
+    });
+})
